7243f614f678a9b9da9adeec352b233382317a98
Celeste McCall <celestemccall93@gmail.com>
2023-02-02 22:56:40 -0600

    Initial Commit

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var celcius = TempConverter.FahrenheeitToCelsius(68);
+            var fahrenheit = TempConverter.CelsiusToFahrenheit(20);
+
+            Console.WriteLine($"Celcius: {celcius}");
+            Console.WriteLine($"");
+            Console.WriteLine($"Fahrenheit:{fahrenheit}");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace StaticExercise
+{
+	public static class TempConverter
+	{
+	
+		public static double FahrenheeitToCelsius(double f)
+		{
+			var results = (f - 32) / 1.8;
+
+			return results;
+		}
+
+		public static double CelsiusToFahrenheit(double c)
+		{
+			var answer = (c * 9) / 5 +  32;
+
+			return answer;
+		}
+	}
+}
+
